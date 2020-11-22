426e11b462f5383cd5e1bfdeb28aa85f1104d7a1
Samuel Gagnon <samuel.gagnon92@gmail.com>
2020-11-22 16:54:45 -0500

    Param pour ne pas avoir de console.

--- make-exec.lisp
+++ make-exec.lisp
@@ -1,4 +1,7 @@
 ;; Loader à partir en partant sbcl dans le shell de emacs
 (load "C:/Users/samue/portacle/config/sbcl-init.lisp")
 (ql:quickload "webcam-autofocus")
-(sb-ext:save-lisp-and-die #p"webcam-autofocus.exe" :toplevel #'webcam-autofocus:main :executable t :compression 9)
+(sb-ext:save-lisp-and-die #p"webcam-autofocus.exe" :toplevel #'webcam-autofocus:main
+                                                   :executable t
+                                                   :compression 9
+                                                   :application-type :gui)
